8ec7b2df31d65e647be4408d1138adb2de6bf45b
crwilliams87 <chris.r.williams@icloud.com>
2014-06-05 15:23:34 +0100

    initial commit

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,3 +1,18 @@
 class Golf
+  def self.hole1(x)
+    a = (1..x).to_a
+    a.map do |i|
+      if i % 21 == 0 then 'plingplong'
+      elsif i % 15 == 0 then 'plingplang'
+      elsif i % 7 == 0 then'plong'
+      elsif i % 5 == 0 then'plang'
+      elsif i % 3 == 0 then 'pling'
+      else i
+      end
+    end
+  end
 
+  def self.hole2(a)
+    a.split.map { |x| x.chr}.join
+  end
 end
